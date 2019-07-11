9e11e5d17a7d05fc972cac867ff4e82e5cd4ee9a
lukefraenza <luke.fraenza@gmail.com>
2019-07-11 12:52:02 -0400

    made it more app-like

--- index.ts
+++ index.ts
@@ -1,27 +1,69 @@
-import { interval, ConnectableObservable } from 'rxjs';
-import { publishLast, publishBehavior, tap, take, refCount } from 'rxjs/operators';
- 
+import { interval, timer, ConnectableObservable, Observable } from 'rxjs';
+import { publishLast, publishBehavior, tap, take, map } from 'rxjs/operators';
+
 console.clear();
 
-const connectable =
-  interval(1000)
-    .pipe(
-      tap(x => console.log("side effect", x)),
-      take(3),
-      publishLast());
- 
-connectable.subscribe(
-  x => console.log(  "Sub. A", x),
-  err => console.log("Sub. A Error", err),
-  () => console.log( "Sub. A Complete"));
- 
- setTimeout(() => {
-connectable.subscribe(
-  x => console.log(  "Sub. B", x),
-  err => console.log("Sub. B Error", err),
-  () => console.log( "Sub. B Complete"));
- },5000);
+/**
+ * MOCK STUFF
+ */
 
- 
+type FilterDataResponse = { foo: number };
 
-(connectable as ConnectableObservable<any>).connect();
+type fakeContext = {
+  stuff?: string,
+  filterData: Observable<FilterDataResponse[]>
+};
+
+// simulation of an API call that takes a second to respond
+const fakeApiCall$ = timer(1000).pipe(
+  take(1),
+  tap(x => console.log('API is being called...')),
+  map(x => {
+    return <FilterDataResponse[]>[{ foo: x }];
+  })
+);
+
+class MockPanel {
+  constructor(public panelName: string){}
+
+  public open(context: fakeContext) {
+    context.filterData.subscribe(
+        x => console.log(`Panel ${this.panelName}:`, x),
+        err => console.log(`Panel ${this.panelName} Error:`, err),
+        () => console.log(`Panel ${this.panelName} Complete`));
+  }
+}
+
+/**
+ * Code example
+ */
+
+
+// this would be a private property in your class
+const filterData$: Observable<FilterDataResponse[]> = fakeApiCall$.pipe(
+  publishLast()
+  );
+
+(filterData$ as ConnectableObservable<any>).connect();
+
+// this is whatever function you have that opens the panel
+function openPanel(panel: MockPanel) {
+  let context = {
+    stuff: "things",
+    filterData: filterData$
+  };
+  console.log(`opening panel ${panel.panelName}...`);
+  panel.open(context);
+}
+
+console.log(`shouldn't call API yet`);
+const panelA = new MockPanel('A');
+const panelB = new MockPanel('B');
+
+
+openPanel(panelA);
+
+
+setTimeout(() => {
+  openPanel(panelB);
+}, 5000);
